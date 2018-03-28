8750f6c0afc56bf5f1576c93a43871225d703c65
Nestor Valentinis <nesti07@gmail.com>
2018-03-28 20:07:13 -0300

    Nestor - Create BookList Container, glue React and Redux

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookLis extends Component {
+class BookLis extends Component {
 
   renderLis() {
     return this.props.books.map( (book) => {
         return (
-          <li key={book.title} c
+          <li key={book.title}
               lassName="list-group-item">
                 {book.title}
           </li>
@@ -14,10 +15,26 @@ export default class BookLis extends Component {
   }
 
   render() {
-      return
+      return (
         <ul className="list-group col-sm-4">
             {this.renderLis()}
         </ul>
       );
   }
 }
+
+// - This function will map the 'app state' from Redux
+//    into the 'props' on the React component
+// - This function will be called by the 'connect' function from Redux
+// - This function will return an object that will be
+//    available as 'props', this.props.books
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+// The Container is exported, not the plain React Component
+// connect receive a function where the map is done
+// When the state change the component will automatically re-rendered
+export default connect(mapStateToProps) (BookLis);
